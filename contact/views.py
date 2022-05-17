31da13552967fe234b982d086812b6d402c3ead4
Yosuke <yousuke@ageet.com>
2022-05-17 18:34:35 +0900

    create form

--- contact/views.py
+++ contact/views.py
@@ -1,16 +1,30 @@
 # project/contact/views.py
 
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import ContactForm
 
 """ 一覧画面"""
 def index(request):
     return render(request, 'contact/index.html')
 
 
-""" お問い合わせフォーム画面"""
 def contact_form(request):
-    return render(request, 'contact/contact_form.html')
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            return redirect('contact:complete')
+
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact_form.html', {'form': form})
+
+
+# """ お問い合わせフォーム画面"""
+# def contact_form(request):
+#     return render(request, 'contact/contact_form.html')
 
 
 """ 送信完了画面"""
